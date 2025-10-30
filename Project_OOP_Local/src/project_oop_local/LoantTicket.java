e17721a3dfe2bff8eaa77677e64dd022852153cc
syduyhoai <syduyhoai111@gmail.com>
2025-10-30 21:18:24 +0700

    add LoantTick.java file

---
+++ Project_OOP_Local/src/project_oop_local/LoantTicket.java
@@ -0,0 +1,14 @@
+
+package project_oop_local;
+
+
+public class LoantTicket {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
