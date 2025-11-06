0da232228a670292fb7bb4238ef1cd8afaab8cb1
Longcodepro <nguyenthanhlong2582006@gmail.com>
2025-11-06 13:11:16 +0700

    File main

--- Librarian.java
+++ Librarian.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Librarian extends Person {
     private String librarianID;
     private String shift;
@@ -24,15 +25,45 @@ public class Librarian extends Person {
     @Override
     public void showINFO(){
         System.out.println("Thong tin thu thu");
-        System.out.println('Name: ' + this.Name);
-        System.out.println('Person ID: ' + this.personID);  
-        System.out.println('Gender: ' + this.gender);
-        System.out.println('Phone Number: ' + this.phoneNumber);        
-        System.out.println('Email: ' + this.email);    
-        System.out.println('Address: ' + this.address);
-        System.out.println('Librarian ID: ' + this.librarianID);     
-        System.out.println('Shift: ' + this.shift);     
-        System.out.println('Salary: ' + this.salary);
-        System.out.println('Password: ' + this.password);
+        System.out.println("Name: " + this.Name);
+        System.out.println("Gender: " + this.gender);
+        System.out.println("Phone Number: " + this.phoneNumber);        
+        System.out.println("Email: " + this.email);    
+        System.out.println("Address: " + this.address);
+        System.out.println("Librarian ID: " + this.librarianID);     
+        System.out.println("Shift: " + this.shift);     
+        System.out.println("Salary: " + this.salary);
+        System.out.println("Password: " + this.password);
+    }
+    // đăng nhập
+    public void Login(){
+          
+    }
+    // làm menu chính cho thủ thư
+    public void menuMain(){
+        Scanner sc = new Scanner(System.in);
+        int lua_chon = 0;
+        do{
+            System.out.println("1. Quan li nguoi doc.");
+            System.out.println("2. Quan li sach.");
+            System.out.println("3. Quan li don hang.");
+            System.out.println("4. Quan li phieu muon.");
+            System.out.println("5. Quan li thu thu.");
+            System.out.println("0. Dang xuat.");
+            System.out.println("Hay nhap vao lua chon cua ban: ");
+            lua_chon = sc.nextInt();
+            // lọc lựa chọn
+            switch(lua_chon){
+                case 1: break;
+                case 2: break;
+                case 3: break;
+                case 4: break;
+                case 5: break;
+                case 0: System.out.println("Xac nhan thhoat"); return;
+                default:
+                 System.out.println("Ban da nhap sai hay nhap lai!");
+                 lua_chon = -1;
+            }
+        }while(lua_chon == -1);
     }
 }
---
+++ Main.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+public class Main{
+    public static void main( String[] args){
+        ArrayList<Librarian> list = new ArrayList<>();
+        list = loadListLib("./data/Librarian.csv");
+        for( int i=0; i<list.size(); i++){
+            list.get(i).showINFO();
+        }
+    }
+
+    public static ArrayList<Librarian> loadListLib (String filename){
+        ArrayList<Librarian> list = new ArrayList<>();
+        try(BufferedReader br = new BufferedReader(new FileReader(filename))){
+            br.readLine();
+            String line;
+            while( (line = br.readLine()) != null ){
+                String[] data = line.split(",");
+                Librarian l = new Librarian( data[0], data[1], data[2], data[3], data[4], data[5], data[6], Double.parseDouble(data[7]), data[8]);
+                list.add(l);
+            }
+        }
+        catch( IOException e){
+            e.printStackTrace();
+        }
+        return list;
+    }
+}
+
+
