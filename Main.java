b1a14a02c0882eb8b827c3b0c3fc656a34bdcec9
Longcodepro <nguyenthanhlong2582006@gmail.com>
2025-11-06 13:42:19 +0700

    Hoan thanh phan login o main.java

--- Main.java
+++ Main.java
@@ -2,15 +2,14 @@ import java.util.*;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 public class Main{
     public static void main( String[] args){
-        ArrayList<Librarian> list = new ArrayList<>();
-        list = loadListLib("./data/Librarian.csv");
-        for( int i=0; i<list.size(); i++){
-            list.get(i).showINFO();
-        }
+        Librarian lib = LoginLibrarian();
+        lib.menuMain();
     }
 
+    // hàm load dữ liệu
     public static ArrayList<Librarian> loadListLib (String filename){
         ArrayList<Librarian> list = new ArrayList<>();
         try(BufferedReader br = new BufferedReader(new FileReader(filename))){
@@ -26,6 +25,52 @@ public class Main{
             e.printStackTrace();
         }
         return list;
+    }
+
+    // hàm đăng nhập
+    public static Librarian LoginLibrarian(){
+        String librarianID;
+        String password;
+        Scanner sc = new Scanner(System.in);
+        boolean flag = false;
+        do{
+            System.out.print("Nhap vao id cua thu thu: ");
+            librarianID = sc.nextLine();
+            System.out.print("Nhap vao password: ");
+            password = sc.nextLine();
+            if(CheckPassword(librarianID, password)){
+                flag = true;
+            }
+        }while(!flag);
+        ArrayList<Librarian> list = new ArrayList<>();
+        list = loadListLib("./data/Librarian.csv");
+        for(int i=0; i<list.size(); i++){
+            if( list.get(i).getLibrarianID().equals(librarianID)){
+                return list.get(i);
+            }
+        } 
+        return null;  
+    }
+
+
+    // hàm kiểm tra password
+    public static boolean CheckPassword(String librarianID, String password){
+        ArrayList<Librarian> list = new ArrayList<>();
+        list = loadListLib("./data/Librarian.csv");
+        for( int i=0; i<list.size(); i++){
+            if( list.get(i).getLibrarianID().equals(librarianID)){
+                if( list.get(i).getPassword().equals(password)){
+                    System.out.println("Dang nhap thanh cong");
+                    return true;
+                }
+                else{
+                    System.out.println("BAN NHAP SAI PASSWORD");
+                    return false;
+                }
+            }
+        }
+        System.out.println("BAN NHAP SAI TEN TAI KHOAN");
+        return false;
     }
 }
 
